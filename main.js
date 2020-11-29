308825a79cd5c3467bcccba62a257c52aeea0c7d
Li Zhiyang <lzypro@gmail.com>
2020-11-29 11:12:28 +0800

    Add GSAP

---
+++ main.js
@@ -0,0 +1,5 @@
+gsap.from(".hero-section", {duration: 1.2, opacity: 0, ease: "power1"});
+gsap.from(".title-caption", {duration: 1, y: 20, opacity: 0, ease: "power1"});
+gsap.from(".smartx-logo", {duration: 1, y: 20, opacity: 0, ease: "power1"});
+gsap.from(".page-title", {duration: 1, delay: 0.2, y: 20, opacity: 0, ease: "power1"});
+gsap.from(".scrolling-arrow", {duration: 1, delay: 0.2, y: 20, opacity: 0, ease: "power1"});
