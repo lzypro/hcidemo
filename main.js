04bdece74199ae513d1dc145a2e441a91fd46b00
Li Zhiyang <claire@Zhiyangs-iMac.local>
2020-11-30 15:18:31 +0800

    add animation

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 gsap.from(".hero-section", {duration: 1.2, opacity: 0, ease: "power1"});
+
 gsap.from(".title-caption", {duration: 1, y: 20, opacity: 0, ease: "power1"});
-gsap.from(".smartx-logo", {duration: 1, y: 20, opacity: 0, ease: "power1"});
-gsap.from(".page-title", {duration: 1, delay: 0.2, y: 20, opacity: 0, ease: "power1"});
-gsap.from(".scrolling-arrow", {duration: 1, delay: 0.2, y: 20, opacity: 0, ease: "power1"});
+
+gsap.from(".smartx-logo", { duration: 1, y: 20, opacity: 0, ease: "power1"});
+
+gsap.from(".page-title", { duration: 1, delay: 0.2, y: 20, opacity: 0, ease: "power1"});
+
+gsap.from(".scrolling-arrow", { duration: 1, delay: 0.2, y: 20, opacity: 0, ease: "power1"});
+
+gsap.to(".scrolling-arrow", {
+    delay: 1.2,
+    keyframes: [{opacity: 0, duration: 0.5, ease:"power1",}, {opacity: 1, duration: 0.5, ease:"power1"}],
+    repeat: 6,
+    });
+
+gsap.from(".concept-title", {scrollTrigger: ".concept-title", duration: 1, delay: 0.5, y: 20, opacity: 0, ease: "power1"});
+
+gsap.from(".concept-text", {scrollTrigger: ".concept-text", duration: 1, delay: 0.7, y: 20, opacity: 0, ease: "power1"});
